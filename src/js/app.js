3a88128f79dbcecc16d60b57132aac2cab7fe3ef
Lordpluha <109725756+Lordpluha@users.noreply.github.com>
2023-01-31 14:18:03 +0200

    Update app.js
    include and use responsive_design.js for layout adaptiveness

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,21 @@
 import * as webpfuncs from './modules/functions.js';
-import Swiper, { Navigation, Pegination} from 'swiper';
-import { animOnScroll } from './modules/beauty_load.js'
+import { animOnScroll } from './modules/beauty_load.js';
+import { ToggleBurger, switchBurger } from './modules/resposive_design.js';
 
-const swiper = new Swiper();
 
-// Красивая загрузка елементов страницы
 document.addEventListener('DOMContentLoaded', function() {
 	webpfuncs.ibg();
 	webpfuncs.testWebP(function () {});
 
+	// Красивая загрузка елементов страницы
 	setTimeout(animOnScroll, 300);
 	window.addEventListener('scroll', animOnScroll);
+
+	// Burger menu
+	switchBurger();
+	// ? Как получить ::before
+	let burger__button = document.querySelector(".menu--burger.nav__menu .menu__button");
+	if (burger__button) {burger__button.addEventListener('click', ToggleBurger);}
 });
+
+window.addEventListener('resize', switchBurger);
