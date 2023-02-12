522586609f5f20ca37a9cb06dfa2c5314241dff6
Lordpluha <109725756+Lordpluha@users.noreply.github.com>
2023-02-12 10:25:08 +0200

    Update resposive_design.js
    detele ;

--- src/js/modules/resposive_design.js
+++ src/js/modules/resposive_design.js
@@ -1,28 +1,24 @@
-function ToggleBurger () {
-	let burger__menu = document.querySelector('.menu--burger.nav__menu');
-	burger__menu.classList.toggle("closed");
-	burger__menu.classList.toggle("open");
-	document.getElementsByTagName("html")[0].classList.toggle("lock");
+export const ToggleBurger = () => {
+	let burger__menu = document.querySelector('.menu--burger.nav__menu')
+	burger__menu.classList.toggle('closed')
+	burger__menu.classList.toggle('open')
+	document.querySelector('html').classList.toggle('lock')
 };
 
-function switchBurger() {
-	let current_height = window.innerHeight;
-	let current_width = window.innerWidth;
-
-	let burger__menu = document.querySelector(".menu.nav__menu");
+export const switchBurger = () => {
+	let current_height = window.innerHeight,
+		current_width = window.innerWidth,
+		burger__menu = document.querySelector('.menu.nav__menu')
 
 	if (current_width <= 840) {
-		burger__menu.classList.remove("menu--default");
-		burger__menu.classList.add("menu--burger");
-		burger__menu.classList.add("closed");
+		burger__menu.classList.remove('menu--default')
+		burger__menu.classList.add('menu--burger')
+		burger__menu.classList.add('closed')
 	} else {
-		burger__menu.classList.remove("menu--burger");
-		burger__menu.classList.remove("open");
-		burger__menu.classList.remove("closed");
-		document.getElementsByTagName("html")[0].classList.remove("lock");
-		burger__menu.classList.add("menu--default");
+		burger__menu.classList.remove('menu--burger')
+		burger__menu.classList.remove('open')
+		burger__menu.classList.remove('closed')
+		document.querySelector('html').classList.remove('lock')
+		burger__menu.classList.add('menu--default')
 	}
 }
-
-export { ToggleBurger };
-export { switchBurger };
