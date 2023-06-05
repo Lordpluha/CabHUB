1ffb445712aed52554a26796f0cc3539726708c4
Vladislav <109725756+Lordpluha@users.noreply.github.com>
2023-06-05 11:09:27 +0300

    Create dialog window .order-now
    - change some links in header, footer and navigation for funtionality

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import * as webpfuncs from './modules/functions.js'
 import { animOnScroll } from './modules/beauty_load.js'
 import { ToggleBurger, switchBurger } from './modules/resposive_design.js'
 
+// Testing browser for supports and some static funcs
 document.addEventListener('DOMContentLoaded', () => webpfuncs.ibg(), webpfuncs.testWebP(()=>{}) )
 
 window.onload = () => {
@@ -9,11 +10,10 @@ window.onload = () => {
 	setTimeout(animOnScroll, 300);
 	window.addEventListener('scroll', animOnScroll);
 
+	// Burger open/close
 	switchBurger();
 	let burger__button = document.querySelector(".menu--burger.nav__menu .menu__button");
 	if (burger__button) {burger__button.addEventListener('click', ToggleBurger);}
-
-
 
 	// Form transform
 	document.querySelector('#date').addEventListener('focus', ()=>event.target.type = 'date')
@@ -27,6 +27,16 @@ window.onload = () => {
 
 	document.querySelector('#end').addEventListener('focus', ()=>event.target.type = 'time')
 	document.querySelector('#end').addEventListener('blur', ()=>event.target.type = 'text')
+
+	// Open OrderNow
+	document.querySelectorAll('a.open-order-now').forEach(el => {
+		el.addEventListener('click', (event) => {
+			event.preventDefault()
+			document.querySelector('dialog.order-now').showModal()
+			document.querySelector('body').classList.add('lock')
+		})
+	})
+
 }
 
 window.onresize = () => {
